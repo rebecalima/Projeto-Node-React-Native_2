eb9a91d52ef7304cbd383f3cba89a4e67369f282
Rebeca Lima <becalg90@gmail.com>
2019-10-06 14:22:08 -0300

    Obtenção da imagem pela api

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -9,6 +9,15 @@ const SpotSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
+}, {
+    /*configurações do mongoose */
+    toJSON: {
+        virtuals: true
+    }
 });
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`
+})
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -19,6 +20,7 @@ mongoose.connect('mongodb+srv://user1:user1@cluster0-hu4dh.mongodb.net/semana-om
 app.use(cors())
 // app.use(cors({ origin: 'http://localhost:3333' })) -> restrição de acesso
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
 app.listen(3333)
